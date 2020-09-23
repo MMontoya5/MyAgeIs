18869727e91e224925cc2ba1f26ca1b3241229b0
MMontoya5 <mateo.montoya@patelhs.org>
2020-09-23 17:43:32 +0000

    I Added integer todaysDate which was 0921

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 class Main {
   public static void main(String[] args) {
     int myAge= 15;
-    int myBirthday= 0108; 
-    System.out.println("Hello world!");
+    int myBirthday= 0108;
+    int todaysDate= 0921 
+    System.out.println(myBirthday == todaysDate);
   }
 }
