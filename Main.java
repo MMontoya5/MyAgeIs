01202038726470212b00c70bbdc695daf043cbaf
MMontoya5 <mateo.montoya@patelhs.org>
2020-09-23 17:28:54 +0000

    I added an integer which was my age which is 15

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
-public static void main(String[] args) {
- int myAge= 15;  
+  public static void main(String[] args) {
+    int myAge= 15;
     System.out.println("Hello world!");
   }
 }
