2852535b90a6c21b4c0377c3b4d3341dcf224402
MMontoya5 <mateo.montoya@patelhs.org>
2020-09-23 17:45:34 +0000

    I added a print statement if today's date and my birthday are equal

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String[] args) {
     int myAge= 15;
     int myBirthday= 0108;
-    int todaysDate= 0921 
+    int todaysDate= 0921; 
     System.out.println(myBirthday == todaysDate);
   }
 }
