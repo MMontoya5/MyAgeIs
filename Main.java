bdc89cfd1db2663a2380d580afb6b7bbf45e1cc8
MMontoya5 <mateo.montoya@patelhs.org>
2020-09-23 17:51:58 +0000

    I added string when printed to say how old I am

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 class Main {
   public static void main(String[] args) {
     int myAge= 15;
-    int myBirthday= 0108;
-    int todaysDate= 0921; 
+    int myBirthday= 108;
+    int todaysDate= 921; 
     System.out.println(myBirthday == todaysDate);
+     System.out.println("I am "+ myAge + " years old" );
   }
 }
